28dae37917f4735adf74f9aeba8107a5f7ab57ce
VSGic <vsgrebnev@aol.com>
2024-07-10 06:58:32 -0500

    update borrestarter

--- borrestarter.py
+++ borrestarter.py
@@ -6,8 +6,7 @@ from requests.auth import HTTPBasicAuth
 from datetime import datetime
 
 polygon_main = "https://your.polygonnode.com"
-polygon_auth = "yoursecretpassword"
-polygon_block_prev = "0"
+polygon_block_prev = 0
 
 def restart_service(service_name):
     try:
@@ -19,34 +18,29 @@ def restart_service(service_name):
 def main(polygon_block_prev):
     service_name = "bor"
     while True:
-        last_block_main = checkbor()
+        last_block_main = get_polygon_local(polygon_main)
         print("polygon_node_last: " + str(last_block_main))
         if last_block_main == polygon_block_prev:
             restart_service(service_name)
             print("Service restarted")
+        else:
+            print("Not necessary to restart")
         polygon_block_prev = last_block_main
-        time.sleep(300)  # Sleep for 5 minutes (300 seconds)
+        time.sleep(180)  # Sleep for 3 minutes (180 seconds)
 
-def get_polygon_yours(polygon_endp, polygon_auth):
+def get_polygon_local(polygon_main):
+    print("Hello")
     block_info = {
         "jsonrpc":"2.0",
         "method":"eth_getBlockByNumber",
         "params":["latest", True],
         "id": 1
         }
-    response = requests.post(f"{polygon_endp}", json=block_info, auth = HTTPBasicAuth('your-username-polygon', polygon_auth))
+    response = requests.post(f"{polygon_main}", json=block_info)
     resp_data = response.json()
     last_block = int(resp_data["result"]["number"],16)
+    print(last_block)
     return last_block
-
-def checkbor():
-    try:
-        last_block_main = "0"
-        last_block_main = get_polygon_yours(polygon_main_2, polygon_auth)
-    except:
-        pass
-
-    return last_block_main
 
 if __name__ == "__main__":
     main(polygon_block_prev)
